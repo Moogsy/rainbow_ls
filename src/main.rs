e7d630c5fcd4e9ff5a87602c3852ed908dfd7176
Saphielle Akiyama <saphielle.akiyama@gmail.com>
2020-12-27 14:34:43 +0100

    spaces

--- src/main.rs
+++ src/main.rs
@@ -16,24 +16,17 @@ fn call_recursive(config: Config, paths: Vec<PathBuf>) {
     let mut stack: VecDeque<PathBuf> = paths.into_iter().collect();
 
     while let Some(path_buf) = stack.pop_front() {
-
         for read_dir in path_buf.read_dir() {
-
             let collected_read_dir: Vec<Result<DirEntry, Error>> = read_dir.into_iter().collect();
-
             display::display_path(&config, &path_buf, &collected_read_dir); 
 
             for dir_entry in collected_read_dir.into_iter().filter_map(Result::ok) {
-
                 if let Ok(metadata) = dir_entry.metadata() {
-
                     let path: PathBuf = dir_entry.path();
 
                     if metadata.file_type().is_dir() {
                         stack.push_back(path);
-
                     } else if metadata.file_type().is_symlink() && config.follow_symlinks {
-
                         if let Ok(link) = fs::read_link(&path) {
                             stack.push_back(link);
                         } else {
